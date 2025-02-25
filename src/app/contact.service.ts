1a600e091983e649d15a93a791d41ba7431e0ba2
Riukari <153175439+Riukari@users.noreply.github.com>
2025-02-25 22:06:35 +0800

    Create contact.service.ts

---
+++ src/app/contact.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Contact } from './contact.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  private contacts: Contact[] = [];
+  private idCounter = 1;
+
+  getContacts(): Contact[] {
+    return this.contacts;
+  }
+
+  addContact(contact: Contact): void {
+    contact.id = this.idCounter++;
+    this.contacts.push(contact);
+  }
+
+  updateContact(updatedContact: Contact): void {
+    const index = this.contacts.findIndex(c => c.id === updatedContact.id);
+    if (index !== -1) {
+      this.contacts[index] = updatedContact;
+    }
+  }
+
+  deleteContact(id: number): void {
+    this.contacts = this.contacts.filter(c => c.id !== id);
+  }
+}
