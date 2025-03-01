5e5129c82fe9f0c292cd3a8de628bd4215883895
Riukari <153175439+Riukari@users.noreply.github.com>
2025-03-01 20:37:29 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ContactListComponent, ContactFormComponent],
+  declarations: [AppComponent, ContactFormComponent, ContactListComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
